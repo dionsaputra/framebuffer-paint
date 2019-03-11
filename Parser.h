cb5a0bef2ee4f87cb5270d7690aeeb1896a3ac35
Adam Fadhel <adamfdhl@gmail.com>
2019-03-11 17:15:27 +0700

    add parser class

---
+++ Parser.h
@@ -0,0 +1,22 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <iostream>
+#include <string>
+#include "Wireframe.h"
+#include "Point.h"
+
+using namespace std;
+
+class Parser {
+private:
+    vector<Wireframe> wireframes;
+    vector<string> arr_str;
+public:
+    Parser();
+    ~Parser();
+    vector<Wireframe> parseFile(string fileName);
+    vector<string> split(string str, string sep);
+};
+
+#endif
